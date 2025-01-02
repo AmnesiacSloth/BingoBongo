767b51adc3b75148b04c44ec02a3b06e47e94a15
Casey Hillers <caseyhillers@gmail.com>
2025-01-01 22:09:24 -0800

    tag using commit sha

--- server/scripts/build_and_run.sh
+++ server/scripts/build_and_run.sh
@@ -4,4 +4,6 @@ docker stop $(docker ps -q --filter ancestor=bingobongo )
 
 docker build . -t bingobongo
 
- docker run -d -it -p 8022:8080 bingobongo
+docker tag bingobongo:latest bingobongo:$(git rev-parse --short HEAD)
+
+docker run -d -it -p 8022:8080 bingobongo
