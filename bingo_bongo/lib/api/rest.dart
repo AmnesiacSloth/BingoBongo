2679db07f2a2019d192b730e3a03fdf85313ffed
Christian V <amnesiacsloth@gmail.com>
2025-01-14 12:02:25 -0800

    implement createUser endpoint

--- bingo_bongo/lib/api/rest.dart
+++ bingo_bongo/lib/api/rest.dart
@@ -3,7 +3,7 @@ import 'package:dio/dio.dart';
 
 class RestApi implements Api {
   final dio = Dio(
-    BaseOptions(baseUrl: "https://dejawhu.chillers.dev"),
+    BaseOptions(baseUrl: "http://192.168.1.111:8081"),
   );
 
   @override
@@ -54,7 +54,14 @@ class RestApi implements Api {
 
   @override
   Future<User> createUser({required String displayName}) async {
-    throw UnimplementedError();
+    final formData = FormData.fromMap({'displayName': displayName});
+    // generate post request with passed args
+    final response = await dio.post("/createUser", data: formData);
+    // parse response as json
+    final json = response.data as JsonMap;
+    // return custom User object with the ID and displayName set from
+    // the returned response (via factory constructor)
+    return (User.fromJson(json));
   }
 
   @override
