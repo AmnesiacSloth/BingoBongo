f2ad599b2cc42923003cdb94ad7db9de621333b1
Christian V <amnesiacsloth@gmail.com>
2025-01-17 09:44:14 -0800

    fix: convert bingoboard to stateful

--- bingo_bongo/lib/widgets/BingoBoard.dart
+++ bingo_bongo/lib/widgets/BingoBoard.dart
@@ -2,19 +2,37 @@ import 'package:flutter/material.dart';
 
 import 'Tile.dart';
 
-Center BingoBoard(BuildContext context) {
-  return Center(
-    child: GridView.count(
-      shrinkWrap: true,
-      padding: EdgeInsets.all(4),
-      childAspectRatio: .5,
-      mainAxisSpacing: 4,
-      crossAxisSpacing: 4,
-      crossAxisCount: 5,
-      physics: NeverScrollableScrollPhysics(),
-      children: List.generate(25, (index) {
-        return Tile(index: index);
-      }),
-    ),
-  );
+class BingoBoard extends StatefulWidget {
+  final String gamename;
+  final List<int> plays;
+  final List<int> layout;
+  const BingoBoard({
+    Key? key,
+    required this.gamename,
+    required this.plays,
+    required this.layout,
+  }) : super(key: key);
+
+  @override
+  State<BingoBoard> createState() => _BingoBoardState();
+}
+
+class _BingoBoardState extends State<BingoBoard> {
+  @override
+  Widget build(BuildContext) {
+    return Center(
+      child: GridView.count(
+        shrinkWrap: true,
+        padding: EdgeInsets.all(4),
+        childAspectRatio: .5,
+        mainAxisSpacing: 4,
+        crossAxisSpacing: 4,
+        crossAxisCount: 5,
+        physics: NeverScrollableScrollPhysics(),
+        children: List.generate(25, (index) {
+          return Tile(index: index);
+        }),
+      ),
+    );
+  }
 }
